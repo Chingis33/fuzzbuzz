769da6c95704e69af309660e6970bd3701fe26c9
Chingis <33646663+Chingis33@users.noreply.github.com>
2019-04-01 15:34:17 +0700

    Update fizzbuzz.php

--- src/fizzbuzz.php
+++ src/fizzbuzz.php
@@ -1,1 +1,13 @@
+<?php
 
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 15 == 0 ) {
+        echo ' FuzzBuzz ';
+    }  elseif ($i % 5 == 0) {
+        echo ' Buzz ';
+    }  elseif ($i % 3 == 0) {
+        echo ' Fuzz ';
+    }  else {
+        echo  ' ' . $i . ' ';
+    }
+}
